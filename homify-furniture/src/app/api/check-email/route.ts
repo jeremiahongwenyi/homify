cad7dd56c458df1d80ad82d2f5b0cd4ac72851ab
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2026-04-02 14:22:23 +0300

    modify check-email api to use prisma

--- homify-furniture/src/app/api/check-email/route.ts
+++ homify-furniture/src/app/api/check-email/route.ts
@@ -1,24 +1,53 @@
 import { NextRequest, NextResponse } from "next/server";
-import { sql } from "@/lib/neon-db";
-import { STATUS_CODES } from "http";
+import { prisma } from "@/lib/prisma";
 
 export async function POST(req: NextRequest) {
   try {
     const { email } = await req.json();
-    const user = await sql`SELECT id FROM "user" WHERE email  = ${email} `;
+    const user = await prisma.user.findUnique({
+      where: { email: email },
+      select: { id: true },
+    });
     console.log("user", user);
-    const exists = user.length > 0;
+
+    const exists = !!user;
 
     return NextResponse.json({
-      data: { exists, email },
+      data: { exists },
       success: true,
     });
   } catch (error) {
-    console.log("error", error);
-    return NextResponse.json({
-      error: { error },
-      statusMessage: 'We could not process your request. Please try again',
-      success: false,
-    },  {status: 500});
+    console.log(error);
+
+    return NextResponse.json(
+      {
+        error: {
+          message: "We could not process your request. Please try again",
+        },
+        success: false,
+      },
+      { status: 500 },
+    );
   }
 }
+
+// export async function POST(req: NextRequest) {
+//   try {
+//     const { email } = await req.json();
+//     const user = await sql`SELECT id FROM "user" WHERE email  = ${email} `;
+//     console.log("user", user);
+//     const exists = user.length > 0;
+
+//     return NextResponse.json({
+//       data: { exists, email },
+//       success: true,
+//     });
+//   } catch (error) {
+//     console.log("error", error);
+//     return NextResponse.json({
+//       error: { error },
+//       statusMessage: 'We could not process your request. Please try again',
+//       success: false,
+//     },  {status: 500});
+//   }
+// }
